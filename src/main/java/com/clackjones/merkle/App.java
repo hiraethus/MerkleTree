b770dbabd5a662a389542ece26531f62b3c0aca2
Mike Clack Jones <mjclackjones@gmail.com>
2017-12-13 21:30:15 +0000

    Initial Commit

---
+++ src/main/java/com/clackjones/merkle/App.java
@@ -0,0 +1,13 @@
+package com.clackjones.merkle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
